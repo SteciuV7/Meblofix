60473c7df25376b759ee7eadf7bfe74d4b0ab14f
SteciuV7 <mynysterbartek@gmail.com>
2025-05-28 23:51:12 +0200

    Aktualizacja aplikacji V.4.00 - Poprawki związane z JWT expiration (Czas sesji na iPhone)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { supabase } from "@/lib/supabase";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
